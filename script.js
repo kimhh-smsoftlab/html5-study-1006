db2e4652f3de6192f4ee10c47e91e9d5e6107dcf
kimhh-smsoftlab <gusgh0910@naver.com>
2020-10-14 15:05:26 +0900

    4 files modified

--- script.js
+++ script.js
@@ -1,2 +1,63 @@
-var x = document.getElementById("demo1");
-//document.getElementById("demo").innerHTML = "dasdasd";
+"use strict";
+let today = new Date();
+let month = today.getMonth() + 1;
+let date = today.getDate();
+let hour = today.getHours();
+let min = today.getMinutes();
+let now = month + "." + date + " " + hour + ":" + min;
+
+{
+  document.getElementById("table_header").innerHTML =
+    '<div class="js_table_tr flex_row">' +
+    "<div>체결시간</div>" +
+    "<div>체결가격(KRW)</div>" +
+    "<div>체결금액(KRW)</div>" +
+    "</div>";
+}
+
+{
+  for (var i = 0; i < 100; i++) {
+    document.getElementById("table_body").innerHTML +=
+      '<div class="js_table_tr flex_row">' +
+      "<div>" +
+      now +
+      "</div>" +
+      "<div>" +
+      getRndInteger(12000000, 13000000) +
+      "</div>" +
+      "<div>" +
+      getRndInteger(20000, 40000000) +
+      "</div>" +
+      "</div>";
+  }
+}
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function addComma(num) {
+  var regexp = /\B(?=(\d{3})+(?!\d))/g;
+  return num.toString().replace(regexp, ",");
+}
+{
+  setInterval(function() {
+    document.getElementById("table_body").innerHTML +=
+      '<div class="js_table_tr flex_row">' +
+      "<div>" +
+      now +
+      "</div>" +
+      "<div>" +
+      getRndInteger(12000000, 13000000) +
+      "</div>" +
+      "<div>" +
+      getRndInteger(20000, 40000000) +
+      "</div>" +
+      "</div>";
+  }, 1000);
+}
+
+var x = document.getElementsByClassName("js_table_tr");
+for (var i = 0; i < x.length; i++) {
+  x[i].className = "js_table_tr flex_row";
+}
