25c1f2bb5297eadf6dc0c22b40984524efcf57b3
kimhh-smsoftlab <gusgh0910@naver.com>
2020-10-16 09:06:42 +0900

    1 file modified

--- project/1015/1015_script.js
+++ project/1015/1015_script.js
@@ -4,8 +4,22 @@ var requestURL = "/project/1015/test.json";
 var request = new XMLHttpRequest();
 request.open("GET", requestURL);
 request.responseType = "json";
-//request.send();
+request.send();
 
 request.onload = function() {
   let data = request.response;
+  createTable(data);
 };
+
+function createTable(jsonObj) {
+  var data = jsonObj.data;
+  var tbody = document.getElementById("table1");
+
+  for (x of data) {
+    tbody.innerHTML += `<tr>
+    <td>${x.time}</td>
+    <td>${x.price}</td>
+    <td>${x.buy_price}</td>
+    </tr>`;
+  }
+}
