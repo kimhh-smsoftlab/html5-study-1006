22a45480d0ff35b1474d60c10c7c26e1f539382c
kimhh-smsoftlab <gusgh0910@naver.com>
2020-10-16 12:24:25 +0900

    2 files modified

--- project/1016/index.js
+++ project/1016/index.js
@@ -1,1 +1,15 @@
-import "./objects.js";
+console.log(window.mastercode);
+var data = window.mastercode;
+const ul;
+console.log(data);
+
+console.log(data[0]);
+console.log("name : ", data[0].name);
+console.clear();
+
+ul = document.getElementById(`find`);
+console.log("name : ", data[0].name);
+for (var x of data) {
+  var li = document.createElement("li");
+  li.textContent = x.name + "---<B>" + x.code;
+}
