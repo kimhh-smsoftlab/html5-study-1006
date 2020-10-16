a8ab625e214a0cfb5a5b5391c540254d67245099
kimhh-smsoftlab <gusgh0910@naver.com>
2020-10-16 12:28:26 +0900

    1 file modified

--- project/1016/index.js
+++ project/1016/index.js
@@ -1,15 +1,17 @@
 console.log(window.mastercode);
 var data = window.mastercode;
-const ul;
+
 console.log(data);
 
 console.log(data[0]);
 console.log("name : ", data[0].name);
 console.clear();
 
-ul = document.getElementById(`find`);
-console.log("name : ", data[0].name);
-for (var x of data) {
-  var li = document.createElement("li");
-  li.textContent = x.name + "---<B>" + x.code;
-}
+try {
+  const ul = document.getElementById(`find`);
+  console.log("name : ", data[0].name);
+  for (var x of data) {
+    var li = document.createElement("li");
+    li.innerHTML = x.name + "---<B>" + x.code;
+  }
+} catch {}
