0ed9f7b7116131bee84c65864dd354e547a9fe79
Kim Hyunho <gusgh0910@gmail.com>
2020-10-19 17:39:33 +0900

    commit

--- project/1019/1019_script.js
+++ project/1019/1019_script.js
@@ -3,8 +3,13 @@ let table;
 let head_title = ["매도잔량","가격", "매수잔량"];
 let chart_5;
 let chart_10;
+let format_data = formatData();
+
+
 createTable();
+insertData(format_data);
 interval();
+new_btn();
 
 function createTable(){
     table = document.getElementById('table');
@@ -27,21 +32,32 @@ function createTableHead(table) {
 function createTableBody(table) {
     let body = document.createElement('tbody');
     body.className = 'table_body'
-    for(row_data of jsondata){
+    // for(row_data of jsondata){
+    for(var i=0; i<20; i++){
         let tr = document.createElement("tr");
-        if(row_data.type == 'sell'){
-            tr.className = `down `;
-            for(var i=0; i<3; i++){
+        if(i<10){
+            if(i<5){
+                tr.className = `down top5`;    
+            }
+            else{
+                tr.className = `down`;
+            }
+            for(var j=0; j<3; j++){
                 let td = document.createElement("td");
-                td.className = "item"+i;
+                td.className = "item"+j;
                 tr.appendChild(td);
             }
         }
         else{
-            tr.className = `up`;
-            for(var i=0; i<3; i++){
+            if(i<15){
+                tr.className = `up`;
+            }
+            else{
+                tr.className = `up top5`;
+            }
+            for(var j=0; j<3; j++){
                 let td = document.createElement("td");
-                td.className = "item"+i;
+                td.className = "item"+j;
                 tr.appendChild(td);
             }
         }
@@ -50,21 +66,73 @@ function createTableBody(table) {
     table.appendChild(body);
 }
 
-function insertData(){
-    var tr_group = document.getElementsByClassName('')
-}
-function interval() {
-    setInterval(function() {
-      change_data(jsondata);
-    }, 2000);
-  }
-
-function change_data(data){
-    for(row_data of data){
-        row_data.Quantity = Math.floor(Math.random() * 10000) + 1;
+function insertData(data){
+    var tr_group_down = document.getElementsByClassName('down');
+    var tr_group_up = document.getElementsByClassName('up');
+    for(var i=0; i<tr_group_down.length; i++){
+        for(var j = 0; j< tr_group_down[i].childNodes.length; j++){
+            tr_group_down[i].childNodes[j].textContent = data[i][j];
+        }
+    }
+    for(var i=0; i<tr_group_up.length; i++){
+        for(var j = 0; j< tr_group_up[9-i].childNodes.length; j++){
+            tr_group_up[9-i].childNodes[j].textContent = data[19-i][j];
+            
+        }
     }
 }
 
-function updatd(){
+function formatData(){
+    var arr = [];
+    for(row_data of jsondata){
+        if(row_data.type == 'sell'){
+            arr.push([row_data.quantity,row_data.price,""]);
+        }
+        else{
+            arr.push(["",row_data.price,row_data.quantity]);
+        }
+    }
+    return arr;
+}
 
+function interval() {
+    setInterval(function() {
+      changeData();
+      new_data = formatData()
+      insertData(new_data);
+    }, 2000);
+  }
+
+function changeData(){
+    for(row_data of jsondata){
+        row_data.quantity = Math.floor(Math.random() * 10000) + 1;
+    }
+}
+
+function new_btn(){
+    chart_5 = document.getElementById('chart_5');
+    chart_5.addEventListener("click", function(){
+        changeView(chart_5)
+    });
+    chart_10 = document.getElementById('chart_10');
+    chart_10.addEventListener("click", function(){
+        changeView(chart_10)
+    });
+}
+
+function changeView(v){
+    var x = document.getElementsByClassName('top5');
+    if(v.id == 'chart_5'){
+        console.log(x);
+        for(y of x){
+            console.log(y);
+            y.style.display = 'none';
+        }
+    }else{
+        console.log("10호가")
+        for(y of x){
+            y.style.display = 'table-row';
+        }
+    }
+    
 }
--- project/1019/object.js
+++ project/1019/object.js
@@ -2,121 +2,121 @@ var data = [
     {
         type:'sell',
         price:3000,
-        Quantity : 1234
+        quantity : 1234
 
     },
     {
         type:'sell',
         price:3000,
-        Quantity : 1234
+        quantity : 1234
 
     },
     {
         type:'sell',
         price:3000,
-        Quantity : 1234
+        quantity : 1234
 
     },
     {
         type:'sell',
         price:3000,
-        Quantity : 1234
+        quantity : 1234
 
     },
     {
         type:'sell',
         price:3000,
-        Quantity : 1234
+        quantity : 1234
 
     },
     {
         type:'sell',
         price:3000,
-        Quantity : 1234
+        quantity : 1234
 
     },
     {
         type:'sell',
         price:3000,
-        Quantity : 1234
+        quantity : 1234
 
     },
     {
         type:'sell',
         price:3000,
-        Quantity : 1234
+        quantity : 1234
 
     },
     {
         type:'sell',
         price:3000,
-        Quantity : 1234
+        quantity : 1234
 
     },
     {
         type:'sell',
         price:3000,
-        Quantity : 1234
+        quantity : 1234
 
     },
     {
         type:'buy',
         price:2000,
-        Quantity : 5678
+        quantity : 5678
 
     },
     {
         type:'buy',
         price:2000,
-        Quantity : 5678
+        quantity : 5678
 
     },
     {
         type:'buy',
         price:2000,
-        Quantity : 5678
+        quantity : 5678
 
     },
     {
         type:'buy',
         price:2000,
-        Quantity : 5678
+        quantity : 5678
 
     },
     {
         type:'buy',
         price:2000,
-        Quantity : 5678
+        quantity : 5678
 
     },
     {
         type:'buy',
         price:2000,
-        Quantity : 5678
+        quantity : 5678
 
     },
     {
         type:'buy',
         price:2000,
-        Quantity : 5678
+        quantity : 5678
 
     },
     {
         type:'buy',
         price:2000,
-        Quantity : 5678
+        quantity : 5678
 
     },
     {
         type:'buy',
         price:2000,
-        Quantity : 5678
+        quantity : 5678
 
     },
     {
         type:'buy',
         price:2000,
-        Quantity : 5678
+        quantity : 5678
 
     }
     
