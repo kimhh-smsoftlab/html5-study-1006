86badc7a3559035302c7ed5098bc316daa910f26
kimhh-smsoftlab <gusgh0910@naver.com>
2020-11-02 18:23:32 +0900

    1102-18:23

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 
 
-inProject(10,20);
-inProject(10,21);
-inProject(10,22);
-inProject(10,22);
-inProject(10,23);
-inProject(10,26);
-inProject(10,27);
-inProject(10,28);
-inProject(10,29);
-inProject(10,30);
+inProject('10','20');
+inProject('10','21');
+inProject('10','22');
+inProject('10','22');
+inProject('10','23');
+inProject('10','26');
+inProject('10','27');
+inProject('10','28');
+inProject('10','29');
+inProject('10','30');
+inProject('11','02');
 
 //날짜를 받고 해당하는 날짜에 링크를 연결한다.
 function inProject(month, day) { // day : string
-  let today = "" + month + day,
+  let today = month + day,
   days = document.getElementsByClassName(`day_${today}`);
       
   Array.from(days).forEach(element => {
