df13065d38bbfb0901fcdc68376039a0ce48907d
kimhh-smsoftlab <gusgh0910@naver.com>
2020-12-10 18:41:45 +0900

    1210-18:41 commit

--- project/12/1209/gulpfile.js
+++ project/12/1209/gulpfile.js
@@ -1,51 +1,12 @@
-// const { series } = require("gulp");
-// const { parallel } = require("gulp");
+const gulp = require("gulp");
 
-// function hello() {
-//     console.log("Hello");
-// }
-
-// function world() {
-//     console.log("World!");
-// }
-
-// function javascript(cb) {
-//     cb();
-// }
-
-// function css(cb) {
-//     cb();
-// }
-
-// function jsMinify(cb) {
-//     cb();
-// }
-
-// function cssMinify(cb) {
-//     cb();
-// }
-
-// // exports.build = series(
-// //     hello,
-// //     parallel(world, series(javascript, css)),
-// //     parallel(jsMinify, cssMinify)
-// // );
-// const clean = function (cb) {
-//     cb();
-// };
-
-// const css1 = series(clean, function (cb) {
-//     cb();
-// });
-
-// const javascript1 = series(clean, function (cb) {
-//     cb();
-// });
-
-// exports.build = parallel(css1, javascript1);
-
-function hello() {
+function hello(done) {
+    done();
     console.log("hello");
 }
 
-exports.default = hello;
+function world(done) {
+    done();
+    console.log("world!!");
+}
+exports.default = gulp.series(hello, world);
